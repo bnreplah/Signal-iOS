1bc853e7dbbb095254d1709977e162256ac40256
Ben <46547251+bnreplah@users.noreply.github.com>
2023-06-29 08:37:40 -0700

    Update appcenter-post-build.sh
    updates to post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#################################################################################
+# Downloading Latest Version of the Wrapper 
+#################################################################################
+
 WRAPPER_VERSION=`curl https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/maven-metadata.xml | grep latest |  cut -d '>' -f 2 | cut -d '<' -f 1`
 if `wget https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-wrappers-java/$WRAPPER_VERSION/vosp-api-wrappers-java-$WRAPPER_VERSION.jar -O VeracodeJavaAPI.jar`; then
                 chmod 755 VeracodeJavaAPI.jar
@@ -7,8 +12,14 @@ if `wget https://repo1.maven.org/maven2/com/veracode/vosp/api/wrappers/vosp-api-
                 exit 1
 fi
 
-#Defined the application name
-Appname="Signal"
+#################################################################################
+# Local Script Variables
+# Edit these to match your application
+#################################################################################
+
+appname="Signal"
+projectLocation="$appname.xcodeproj"
+
 
 brew tap veracode/tap
 brew install gen-ir
@@ -16,27 +27,33 @@ brew install gen-ir
 pod install
 
 
-#if including the SRCCLR_API_TOKEN as an enviornmental variable to be able to conduct Veracode SCA Agent-based scan
-echo "RUNNING VERACODE SCA AGENT-BASED SCAN"
-(curl -sSL https://download.sourceclear.com/ci.sh) | sh - scan $APPCENTER_SOURCE_DIRECTORY
-
 #APPCENTER DEFINED ENV VAR
-echo "APPCENTER_XCODE_PROJECT:  $APPCENTER_XCODE_PROJECT"	
+echo "APPCENTER_XCODE_PROJECT/WORKSPACE:  $APPCENTER_XCODE_PROJECT"	
 echo "APPCENTER_XCODE_SCHEME: $APPCENTER_XCODE_SCHEME"
 echo "APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY"
 ls $APPCENTER_SOURCE_DIRECTORY
 echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
-xcodebuild archive -workspace $Appname.xcworkspace -configuration Debug -scheme $APPCENTER_XCODE_SCHEME -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath $Appname.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
+xcodebuild archive -workspace $appname.xcworkspace -configuration Debug -scheme $APPCENTER_XCODE_SCHEME -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath $appname.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
+
+
+#if including the SRCCLR_API_TOKEN as an enviornmental variable to be able to conduct Veracode SCA Agent-based scan
+echo "RUNNING VERACODE SCA AGENT-BASED SCAN"
+SRCCLR_API_TOKEN=$SRCCLR_API_TOKEN
+curl -sSL https://download.sourceclear.com/ci.sh | sh
+
+
 
 ls -la 
-gen-ir build_log.txt $Appname.xcarchive/IR
-zip -r $Appname.zip $Appname.xcarchive
-zip -r $Appname-SCA.zip Podfile.lock Gemfile.lock Pods/
+#gen-ir build_log.txt $appname.xcarchive/IR
+#updated version
+gen-ir build_log.txt $appname.xcarchive/ --project-path $projectLocation 
+zip -r $appname.zip $appname.xcarchive
+zip -r $appname-Podfile.zip Podfile.lock Gemfile.lock Pods/
 ls -la
-mkdir artifacts/
+mkdir Veracode/
 ls -la
-cp $Appname-SCA.zip $Appname.zip artifacts/
-ls -la artifacts/
+cp $Appname-Podfile.zip $Appname.zip Veracode/
+ls -la Veracode/
 
-java -jar VeracodeJavaAPI.jar -action UploadAndScan -vid $VID -vkey $VKEY  -deleteincompletescan 2 -createprofile false  -appname "Gen-IR pipeline"  -version "v0.3.APPCENTER" -filepath artifacts/Signal.zip
+java -jar VeracodeJavaAPI.jar -action UploadAndScan -vid $VID -vkey $VKEY  -deleteincompletescan 2 -createprofile false  -appname "Gen-IR pipeline"  -version "$APPCENTER_BUILD_ID-v0.3.APPCENTER" -filepath Veracode/
 
