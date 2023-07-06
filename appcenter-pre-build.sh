6c8045d2c2936f2e7086def9f7f705ffbec04634
Ben <46547251+bnreplah@users.noreply.github.com>
2023-07-06 11:36:37 -0700

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PREBUILD SCRIPT
 echo "Exporting ENV Variables prebuild"
-export DEBUG_INFORMATION_FORMAT=dwarf-with-dsym
-export CODE_SIGN_IDENTITY="" 
-export CODE_SIGNING_REQUIRED=NO 
-export CODE_SIGNING_ALLOWED=NO 
-export ENABLE_BITCODE=NO
+#export DEBUG_INFORMATION_FORMAT=dwarf-with-dsym
+#export CODE_SIGN_IDENTITY="" 
+#export CODE_SIGNING_REQUIRED=NO 
+#export CODE_SIGNING_ALLOWED=NO 
+#export ENABLE_BITCODE=NO
