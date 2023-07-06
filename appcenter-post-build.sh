b52ef5ab85d0c48abc2cc6db08ec9982b59bb740
Ben <46547251+bnreplah@users.noreply.github.com>
2023-07-06 14:28:14 -0700

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -20,6 +20,7 @@ fi
 appname="Signal"
 projectLocation="$appname.xcodeproj"
 debug=true
+track=0
 
 echo "========================================================================================================================================================================"
 echo "Clean build"
@@ -52,57 +53,58 @@ echo "APPCENTER_OUTPUT_DIRECTORY: $APPCENTER_OUTPUT_DIRECTORY"
 echo "========================================================================================================================================================================"
 echo "creating archive"
 echo "========================================================================================================================================================================"
+
+
 xcodebuild archive -workspace $appname.xcworkspace -configuration Debug -scheme $APPCENTER_XCODE_SCHEME -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath $appname.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
 cat build_log.txt
 
 if [$debug]; then
       xcodebuild archive -workspace Signal.xcworkspace  -configuration Debug -scheme Signal-Veracode -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym -archivePath Signal.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO ENABLE_BITCODE=NO | tee build_log.txt
       echo "========================================================================================================================================================================"
-      echo "Output from Build_log.txt"
+      echo "Output from Build_log.txt #############################################################################################################################################"
       echo "========================================================================================================================================================================"
       cat build_log.txt
 fi
 
 #if including the SRCCLR_API_TOKEN as an enviornmental variable to be able to conduct Veracode SCA Agent-based scan
 echo "========================================================================================================================================================================"
-echo "RUNNING VERACODE SCA AGENT-BASED SCAN"
+echo "RUNNING VERACODE SCA AGENT-BASED SCAN  #################################################################################################################################"
 echo "========================================================================================================================================================================"
 SRCCLR_API_TOKEN=$SRCCLR_API_TOKEN
 curl -sSL https://download.sourceclear.com/ci.sh | sh
-
-
-
 ls -la 
+
 #gen-ir build_log.txt $appname.xcarchive/IR
 #updated version
 echo "========================================================================================================================================================================"
-echo "GEN-IR Running ##############################################################################"
+echo "GEN-IR Running #########################################################################################################################################################"
 echo "========================================================================================================================================================================"
 gen-ir build_log.txt $appname.xcarchive/ --project-path $projectLocation 
 
 echo "========================================================================================================================================================================" 
-echo "Contents of archive"
+echo "Contents of archive ####################################################################################################################################################"
 echo "========================================================================================================================================================================"
 
 ls -la $appname.xcarchive
 
 if [$debug]; then
   echo "========================================================================================================================================================================" 
-  echo "Running modified version to write bitcode out to IR folder"
+  echo "Running modified version to write bitcode out to IR folder #############################################################################################################"
   echo "========================================================================================================================================================================"
   gen-ir build_log.txt Signal.xcarchive/IR --project-path ./Signal.xcodeproj
   
-  ls -la $appname.xcarchive
-
-  gen-ir build_log.txt Signal.xcarchive/IR
+  #ls -la $appname.xcarchive
+  
+  #gen-ir build_log.txt Signal.xcarchive/IR
 
   ls -la $appname.xcarchive/IR
 fi
 
 
 echo "========================================================================================================================================================================"
-echo "Zipping up artifact"
+echo "Zipping up artifact ####################################################################################################################################################"
 echo "========================================================================================================================================================================"
+
 zip -r $appname.zip $appname.xcarchive
 zip -r $appname-Podfile.zip Podfile.lock Gemfile.lock Pods/
 ls -la
@@ -111,6 +113,12 @@ mkdir Veracode/
 ls -la
 cp $appname-Podfile.zip $appname.zip Veracode/
 ls -la Veracode/
+
+
+echo "========================================================================================================================================================================"
+echo "Veracode Upload and Scan  ##############################################################################################################################################"
+echo "========================================================================================================================================================================"
+
 
 java -jar VeracodeJavaAPI.jar -action UploadAndScan -vid $VID -vkey $VKEY  -deleteincompletescan 2 -createprofile false -createsandbox true -appname "Gen-IR pipeline" -sandboxname "MSAPPCENTER" -version "$APPCENTER_BUILD_ID-v0.3.APPCENTER" -filepath Veracode/
 
